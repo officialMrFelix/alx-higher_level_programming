e3f26329a9bef61c4c4dd6ea7d539f780c203489
officialMrFelix <felixobianozie@hotmail.com>
2022-08-26 01:47:19 +0000

    Included calculator_1.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,24 +4,24 @@ from calculator_1 import add, sub, mul, div
 
 
 def main():
-    number_of_arg = len(argv) - 1
-    if number_of_arg < 3 or number_of_arg > 3:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-    else:
-        if argv[1] != '+' or argv[1] != '-' or argv[1] != '*' or argv[1] != '/':
+        print("debug_argv {}".format(len(argv)))
+        #if argv[2] != '+' or argv[2] != '-' or argv[2] != '*' or argv[2] != '/':
+        if argv[2] not in ['+', '-', '*', '/']:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
         else:
-            if argv[1] == '+':
-                print("{} + {} = {}".format(argv[0], argv[2], add(int(argv[0]), int(argv[2]))))
-            elif argv[1] == '-':
-                print("{} + {} = {}".format(argv[0], argv[2], sub(int(argv[0]), int(argv[2]))))
-            elif argv[1] == '*':
-                print("{} + {} = {}".format(argv[0], argv[2], mul(int(argv[0]), int(argv[2]))))
-            elif argv[1] == '/':
-                print("{} + {} = {}".format(argv[0], argv[2], div(int(argv[0]), int(argv[2]))))
+            if argv[2] == '+':
+                print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+            elif argv[2] == '-':
+                print("{} - {} = {}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+            elif argv[2] == '*':
+                print("{} * {} = {}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+            elif argv[2] == '/':
+                print("{} / {} = {}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
 
 
 if __name__ == "__main__":
