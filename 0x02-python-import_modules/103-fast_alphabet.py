c7763adb39b0f6994fc275e163c43a25919b103d
officialMrFelix <felixobianozie@hotmail.com>
2022-08-26 01:56:41 +0000

    More updates

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-import string
-print(string.ascii_uppercase)
+
+
+if __name__ == "__main__":    
+    for i in range(65, 91):
+        print("{}".format(chr(i)), end="")
+    print()
