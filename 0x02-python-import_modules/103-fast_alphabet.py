11df260746abd0f4fae08f1665357dede40a8b4a
officialMrFelix <felixobianozie@hotmail.com>
2022-08-26 02:49:09 +0000

    Bug fix: 103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-
-if __name__ == "__main__":    
-    for i in range(65, 91):
-        print("{}".format(chr(i)), end="")
-    print()
+import string
+print(string.ascii_uppercase)
