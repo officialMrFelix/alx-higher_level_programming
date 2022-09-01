e5a03a2d16b873af97cd023ac86f6e3ab6684926
officialMrFelix <felixobianozie@hotmail.com>
2022-09-01 02:12:41 +0000

    Included 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string is not None:
+        new_string = ''
+        for i in my_string:
+            if (i != 'c') and (i != 'C'):
+                new_string += i
+        return new_string
